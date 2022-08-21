4cd45883528d5e15864e7db9e322915e94b90b4b
html2021 <78289310+html2021@users.noreply.github.com>
2022-08-21 10:46:54 +0900

    Delete mail.js

--- mail.js
+++
@@ -1,5 +0,0 @@
-function main() {
-    name1 = document.getElementById("name").value;
-    text1 = document.getElementById("message").value;
-    open( "mailto:example.com?subject="+name1+"&body="+text1,"_blank") ;
-}
